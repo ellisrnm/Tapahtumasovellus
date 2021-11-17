a3f2c86e114d2cb506f7c6e44eecd5907068d5ce
Elli Saarenmaa <47243975+ellisrnm@users.noreply.github.com>
2021-11-17 22:40:30 +0200

    Add database schema to schema.sql

---
+++ schema.sql
@@ -0,0 +1,71 @@
+CREATE TABLE Users (
+    user_id SERIAL,
+    username TEXT,
+    password TEXT,
+    isadmin BOOLEAN,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE Events (
+    event_id SERIAL,
+    creator_id INTEGER,
+    isprivate BOOLEAN,
+    event_description TEXT,
+    place TEXT,
+    event_date DATE,
+    start_time TIME,
+    end_time TIME,
+    category TEXT,
+    city TEXT,
+    PRIMARY KEY (event_id),
+    FOREIGN KEY (creator_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Categories (
+    category_id SERIAL,
+    category_name TEXT,
+    category_description TEXT,
+    PRIMARY KEY (category_id)
+);
+
+CREATE TABLE Invitees (
+    event_id INTEGER,
+    user_id INTEGER,
+    PRIMARY KEY (event_id, user_id),
+    FOREIGN KEY (event_id) REFERENCES Events(event_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Attendance (
+    event_id INTEGER,
+    user_id INTEGER,
+    attending BOOLEAN,
+    PRIMARY KEY (event_id, user_id),
+    FOREIGN KEY (event_id) REFERENCES Events(event_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Groups (
+    group_id SERIAL,
+    group_name TEXT,
+    group_description TEXT,
+    PRIMARY KEY (group_id)
+);
+
+CREATE TABLE Members (
+    group_id INTEGER,
+    user_id INTEGER,
+    PRIMARY KEY (group_id, user_id),
+    FOREIGN KEY (group_id) REFERENCES Groups(group_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Comments (
+    comment_id SERIAL,
+    comment TEXT,
+    event_id INTEGER,
+    author_id INTEGER,
+    PRIMARY KEY (comment_id),
+    FOREIGN KEY (author_id) REFERENCES Users(user_id),
+    FOREIGN KEY (event_id) REFERENCES Events(event_id)
+);
